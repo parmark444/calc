73022017b903c83c15f243e35824a3af72522c23
Kush Parmar <parmkus26@vt.edu>
2023-12-31 15:34:17 -0800

    Add project files.

---
+++ calc/Calc.cs
@@ -0,0 +1,28 @@
+﻿class Calc 
+{
+    public static double DoOperation(double x, double y, string operation) 
+    {
+        double result = double.NaN;
+        switch (operation) 
+        {
+            case "add":
+                result = x + y;
+                break;
+            case "sub":
+                result = x - y;
+                break;
+            case "mult":
+                result = x * y;
+                break;
+            case "div":
+                if (y != 0) 
+                {
+                    result = x / y;
+                }
+                break;
+            default:
+                break;
+        }
+        return result;
+    }
+}
